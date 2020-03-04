17bb089c5f6bb51fe643b3ef6a88dc0c78deed42
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-04 22:06:06 +0100

    basic navigation with styles implemented

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -9,8 +9,35 @@ return (
     <div className="header">
       <div className="header__image"></div>
       <div className="header__login-nav-content-container">
-        <div className="header__login-nav-container"></div>
-        <div className="header__content-container"></div>
+        <div className="header__login-nav-container">
+          <div className="header__login">
+            <span>Zaloguj</span>
+            <span>Załóż konto</span>
+          </div>
+          <div className="header__nav">
+            <span>Start</span>
+            <span>O co chodzi?</span>
+            <span>O nas</span>
+            <span>Fundacja i organizacje</span>
+            <span>Kontakt</span>
+          </div>
+        </div>
+        <div className="header__content-container">
+          <div className="header__sloganbox">
+            <span>Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</span>
+          </div>
+          <div className="header__decorationbox">
+            <span>Tu będzie ładna dekoracja</span>
+          </div>
+          <div className="header__buttons-section">
+            <div>
+              Tu będzie guzik oddaj rzeczy
+            </div>
+            <div>
+              Tu będzie guzik zorganizuj zbiórkę
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </>
