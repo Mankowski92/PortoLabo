e477c7e3b25c7deed538bfb6983f435c8f3ebd9e
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-05 14:30:53 +0100

    basic styles and html structure for second component established

--- src/components/Home/ThreeColumns.js
+++ src/components/Home/ThreeColumns.js
@@ -7,7 +7,41 @@ const ThreeColumns = () => {
 return (
 
   <>
-  
+    <div className="three-columns">
+      <div className="three-columns__column-container">
+        <div className="column-container__numberbox">
+          
+        </div>
+        <div>
+          
+        </div>
+        <div>
+          
+        </div>
+      </div>
+      <div className="three-columns__column-container">
+        <div className="column-container__numberbox">
+          
+        </div>
+        <div>
+          
+        </div>
+        <div>
+          
+        </div>
+      </div>
+      <div className="three-columns__column-container">
+        <div className="column-container__numberbox">
+          
+        </div>
+        <div>
+            
+        </div>
+        <div>
+            
+        </div>
+      </div>
+    </div>
   </>
   );
 }
