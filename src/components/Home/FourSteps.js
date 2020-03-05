044acc1e326434a65636f6c59521d57c6d1c4388
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-05 15:49:33 +0100

    3 columns 90% done and section fourSteps (new component) added

--- src/components/Home/FourSteps.js
+++ src/components/Home/FourSteps.js
@@ -5,11 +5,62 @@ const FourSteps = () => {
     
 return (
 
-  <>
-    <div className="FourSteps">
-        
+<div>
+    <div className="four-steps">
+        <div className="four-steps__title">Wystarczą 4 proste kroki</div>
+        <div className="four-steps__decorationbox"></div>
+            <div className="four-steps__background-for-step-by-step">
+            <div className="four-steps__step-by-step-container">
+                <div className="step-by-step-container__each-step-container">
+                    <div className="each-step-container__graphic">
+                        
+                    </div>
+                    <div className="each-step-container__action-title">
+                        Wybierz rzeczy
+                    </div>
+                    <div className="each-step-container__action-description">
+                        ubrania, zabawki, sprzęt i inne
+                    </div>
+                </div>
+                <div className="step-by-step-container__each-step-container">
+                    <div className="each-step-container__graphic">
+                        
+                    </div>
+                    <div className="each-step-container__action-title">
+                        Spakuj je
+                    </div>
+                    <div className="each-step-container__action-description">
+                        skorzystaj z worków na śmieci
+                    </div>
+                </div>
+                <div className="step-by-step-container__each-step-container">
+                    <div className="each-step-container__graphic">
+                        
+                    </div>
+                    <div className="each-step-container__action-title">
+                        Zdecyduj komu chcesz pomóc
+                    </div>
+                    <div className="each-step-container__action-description">
+                        wybierz zaufane miejsce
+                    </div>
+                </div>
+                <div className="step-by-step-container__each-step-container">
+                    <div className="each-step-container__graphic">
+                        
+                    </div>
+                    <div className="each-step-container__action-title">
+                        Zamów kuriera
+                    </div>
+                    <div className="each-step-container__action-description">
+                        kurier przyjedzie w dogodnym terminie
+                    </div>     
+                </div>
+            </div>
+        </div>  
+        <div className="four-steps__button"></div>    
     </div>
-  </>
+</div>
+  
   );
 }
 
