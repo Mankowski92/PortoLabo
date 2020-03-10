40abca48e64e53588010b96bff4a48ab8f6aef76
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-10 14:26:59 +0100

    build established to implement react scroll

--- src/components/Home/ContactFooter.js
+++ src/components/Home/ContactFooter.js
@@ -6,7 +6,13 @@ const ContactFooter = () => {
 return (
     <>
         <div className="contact-footer-container">
-            SECTION CONTACT FOOTER
+            <div className="contact__container">
+                <div className="contact__title">Skontaktuj się z nami</div>
+                <div className="contact_decoration-box"></div>
+                <div className="contact__name-and-email"></div>
+                <div className="contact__message"></div>
+                <div className="button_button"></div>
+            </div>
         </div>
     </>
 );
