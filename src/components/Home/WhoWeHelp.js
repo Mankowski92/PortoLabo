5675e37dd90459075e90a4d4abdc34b3315bac8b
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-10 13:27:43 +0100

    section WhoWeHelp done, without fundations section

--- src/components/Home/WhoWeHelp.js
+++ src/components/Home/WhoWeHelp.js
@@ -8,9 +8,17 @@ return (
         <div className="who-we-help-container">
             <div className="who-we-help__title">Komu pomagamy?</div>
             <div className="who-we-help__decoration"></div>
-            <div className="who-we-help__entities"></div>
-            <div className="who-we-help__text"></div>
-            <div className="who-we-help__main-content"></div>
+            <div className="who-we-help__entities">
+                <div>Fundacjom</div>
+                <div>Organizacjom pozarządowym</div>
+                <div>Lokalnym zbiórkom</div>
+            </div>
+            <div className="who-we-help__text">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</div>
+            <div className="who-we-help__main-content">
+                <div>Fundation 1</div>
+                <div>Fundation 2</div>
+                <div>Fundation 3</div>
+            </div>
         </div>
     </>
 );
