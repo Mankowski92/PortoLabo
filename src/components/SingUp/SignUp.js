d50b103170e9f6b2b2353579460e7b0d9637a1f9
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-11 14:59:09 +0100

    singup structure and scss classes established

--- src/components/SingUp/SignUp.js
+++ src/components/SingUp/SignUp.js
@@ -1,15 +1,27 @@
 import React from "react";
-import Header from "../Home/Header";
+import Navigation from "../Navigation";
 
-
-const SignUp = () => {
+const Login = () => {
     
 return (
 
-  <>
-    <Header />
-  </>
+  <div  className="Sing-up-container">
+    <Navigation />
+    <div className="Sing-up-container__login-section">
+      <div className="Sing-up-section__login-text">Zaloguj się</div>
+      <div className="Sing-up-section__decoration-box"></div>
+      <div className="Sing-up-section__email-password-container">
+        <div className="Sing-up__email-password-container__email">Email</div>
+        <div className="Sing-up__email-password-container__password">Hasło</div>
+        <div className="Sing-up__email-password-container__password-repeat">Powtórz hasło</div>
+      </div>  
+      <div className="Sing-up-section__buttons-section">
+        <span>Załóż konto</span>
+        <span>Zaloguj się</span>
+      </div>
+    </div>
+  </div>
   );
 }
 
-export default SignUp
+export default Login
