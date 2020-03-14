ad4dea334737a5e801a147923db223cd9bcf7b82
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-14 17:07:17 +0100

    jsx structure build fot LogOut component

--- src/components/LogOut/LogOut.js
+++ src/components/LogOut/LogOut.js
@@ -7,18 +7,10 @@ return (
 
   <div  className="log-out-container">
     <Navigation />
-    <div className="log-out-container__login-section">
-      <div className="log-out-section__login-text">Załóż konto</div>
-      <div className="log-out-section__decoration-box"></div>
-      <div className="log-out-section__email-password-container">
-        <div className="log-out__email-password-container__email">Email</div>
-        <div className="log-out__email-password-container__password">Hasło</div>
-        <div className="log-out__email-password-container__password-repeat">Powtórz hasło</div>
-      </div>  
-      <div className="log-out-section__buttons-section">
-        <span>Załóż konto</span>
-        <span>Zaloguj się</span>
-      </div>
+    <div className="log-out-container__content-container">
+      <div className="content-container__title">Wylogowanie nastąpiło pomyślnie!</div>
+      <div className="content-container__decoration"></div>
+      <div className="content-container__button"></div>
     </div>
   </div>
   );
