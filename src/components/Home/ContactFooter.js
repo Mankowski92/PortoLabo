1eaf440a3b5f5612d2f66ebb0e1afa23b5767cae
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-16 23:12:20 +0100

    final change today (cleaning code) in ContactFooter

--- src/components/Home/ContactFooter.js
+++ src/components/Home/ContactFooter.js
@@ -16,7 +16,6 @@ const ContactFooter = () => {
             "name": name.value,
             "email": email.value,
             "text": text.value
-
         }
       })
     }
@@ -59,8 +58,7 @@ return (
                         onChange={e => setText(e.target.value)}
                         placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
                     </div>
-                    <button type="submit" onClick={handleSubmit}>NACIŚNIJ MIĘ</button>
-                    {/* <div className="form__button" onClick={handleSubmit}>Wyślij</div> */}
+                    <div className="form__button" type="submit" onClick={handleSubmit}>Wyślij</div>
                 </form>
             </div>
             <div className="contact__over">
