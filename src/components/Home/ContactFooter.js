6f173a59237813401cae0a78d13e58a446c39df9
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-16 20:48:41 +0100

    endpoint broken. Work on this component will be continue after endpoint repair

--- src/components/Home/ContactFooter.js
+++ src/components/Home/ContactFooter.js
@@ -59,7 +59,8 @@ return (
                         onChange={e => setText(e.target.value)}
                         placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
                     </div>
-                    <div className="form__button" onClick={handleSubmit}>Wyślij</div>
+                    <button type="submit" onClick={handleSubmit}>NACIŚNIJ MIĘ</button>
+                    {/* <div className="form__button" onClick={handleSubmit}>Wyślij</div> */}
                 </form>
             </div>
             <div className="contact__over">
