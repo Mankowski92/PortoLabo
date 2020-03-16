093b0e21443d42c8071ccdf17781f92a1f6875f0
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-16 18:01:19 +0100

    checkpoint

--- src/components/LogOut/LogOut.js
+++ src/components/LogOut/LogOut.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Navigation from "../Navigation";
+import { Link as LinkTo } from "react-router-dom";
 
 const SignIn = () => {
     
 return (
 
-  <div  className="log-out-container">
+  <div className="log-out-main-div">
     <Navigation />
-    <div className="log-out-container__content-container">
-      <div className="content-container__title">Wylogowanie nastąpiło pomyślnie!</div>
-      <div className="content-container__decoration"></div>
-      <div className="content-container__button"></div>
+    <div  className="log-out-container">
+      <div className="log-out-container__content-container">
+        <div className="content-container__title">Wylogowanie nastąpiło pomyślnie!</div>
+        <div className="content-container__decoration"></div>
+        <LinkTo to="/"><div className="content-container__button">Strona główna</div></LinkTo>
+      </div>
     </div>
   </div>
+
   );
 }
 
