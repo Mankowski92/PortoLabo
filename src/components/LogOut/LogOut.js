1af87864d7b8d343a39ae9c125c44b7a9a24cd82
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-20 22:50:09 +0100

    component code format

--- src/components/LogOut/LogOut.js
+++ src/components/LogOut/LogOut.js
@@ -3,21 +3,22 @@ import Navigation from "../Navigation";
 import { Link as LinkTo } from "react-router-dom";
 
 const SignIn = () => {
-    
-return (
-
-  <div className="log-out-main-div">
-    <Navigation />
-    <div  className="log-out-container">
-      <div className="log-out-container__content-container">
-        <div className="content-container__title">Wylogowanie nastąpiło pomyślnie!</div>
-        <div className="content-container__decoration"></div>
-        <LinkTo to="/"><div className="content-container__button">Strona główna</div></LinkTo>
+  return (
+    <div className="log-out-main-div">
+      <Navigation />
+      <div className="log-out-container">
+        <div className="log-out-container__content-container">
+          <div className="content-container__title">
+            Wylogowanie nastąpiło pomyślnie!
+          </div>
+          <div className="content-container__decoration"></div>
+          <LinkTo to="/">
+            <div className="content-container__button">Strona główna</div>
+          </LinkTo>
+        </div>
       </div>
     </div>
-  </div>
-
   );
-}
+};
 
-export default SignIn
+export default SignIn;
