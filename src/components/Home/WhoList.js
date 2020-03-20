53e81a251771f977538bbafe76d6a5441b6e27a0
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-20 22:42:24 +0100

    WhoList component code format

--- src/components/Home/WhoList.js
+++ src/components/Home/WhoList.js
@@ -1,54 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const WhoList = ({ fundation }) => {
+  const [data, setData] = useState(false);
+  const [filter, setFilter] = useState([]);
 
-    const [data, setData] = useState(false);
-    const [filter, setFilter] =useState([])
+  useEffect(() => {
+    if (data) {
+      setFilter(data.foundations);
+      console.log(filter); //////////MODS HERE
+    }
+  }, [fundation]);
 
-    useEffect(() => {
-      if (data) {
-        setFilter(data.foundations)
-        console.log(filter)      //////////MODS HERE
-      }}, [fundation])
-      
-      
+  useEffect(() => {
+    fetch(`https://api.jsonbin.io/b/5e69461fe835bb6a77a18247/7`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "secret-key":
+          "$2b$10$u8DPHjDCkFgde26MlWEb3O7/ERBULwxzQHDBlS3.HL3Zok7YPW4cC"
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Błąd sieci!");
+        }
+      })
+      .then(data => {
+        setData(data);
+      })
+      .catch(err => {
+        console.error("Pojawiły się błędy - " + err.message);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(`https://api.jsonbin.io/b/5e69461fe835bb6a77a18247/7`,
-        {
-            method: "GET",
-            headers: {  
-            "Content-Type": "application/json",
-            "secret-key": "$2b$10$u8DPHjDCkFgde26MlWEb3O7/ERBULwxzQHDBlS3.HL3Zok7YPW4cC"
-              } 
-            })
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            } else {
-              throw new Error("Błąd sieci!");
-            }
-          })
-          .then(data => {
-            setData(data);
-          })
-          .catch(err => {
-            console.error("Pojawiły się błędy - " + err.message);
-          });
-      }, []);
-
- if (!data) {
+  if (!data) {
     return <h1>Ładuje...</h1>;
   }
   return (
     <div className="entity-container">
       <div className="entity-name-and-description">
-    <div className="entity-name-and-description__name"> {data.foundations[0].name}</div>
-        <div className="entity-name-and-description__description">{data.foundations[0].description}</div>
+        <div className="entity-name-and-description__name">
+          {" "}
+          {data.foundations[0].name}
+        </div>
+        <div className="entity-name-and-description__description">
+          {data.foundations[0].description}
+        </div>
       </div>
-    <div className="entity-additional-info">{data.foundations[0].status}</div>
-    </div>  
-  )
-}
+      <div className="entity-additional-info">{data.foundations[0].status}</div>
+    </div>
+  );
+};
 
-export default WhoList
+export default WhoList;
