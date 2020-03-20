f171fd33feeae6d1940f44abef1d697f9e677907
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-20 22:47:13 +0100

    ThreeColumns component code format

--- src/components/Home/ThreeColumns.js
+++ src/components/Home/ThreeColumns.js
@@ -1,49 +1,40 @@
 import React from "react";
 
-
-
 const ThreeColumns = () => {
-    
-return (
-
-  <>
-    <div className="three-columns" id="start">
-      <div className="three-columns__column-container">
-        <div className="column-container__numberbox">
-          10
+  return (
+    <>
+      <div className="three-columns" id="start">
+        <div className="three-columns__column-container">
+          <div className="column-container__numberbox">10</div>
+          <div className="column-container__description">ODDANYCH WORKÓW</div>
+          <div className="column-container__text">
+            Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+            enim a elit viverra elementuma. Aliquam erat volutpat.
+          </div>
         </div>
-        <div className="column-container__description">
-          ODDANYCH WORKÓW
+        <div className="three-columns__column-container">
+          <div className="column-container__numberbox">5</div>
+          <div className="column-container__description">
+            WSPARTYCH ORGANIZACJI
+          </div>
+          <div className="column-container__text">
+            Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+            enim a elit viverra elementuma. Aliquam erat volutpat.
+          </div>
         </div>
-        <div className="column-container__text">
-          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
-        </div>
-      </div>
-      <div className="three-columns__column-container">
-        <div className="column-container__numberbox">
-          5
-        </div>
-        <div className="column-container__description">
-          WSPARTYCH ORGANIZACJI
-        </div>
-        <div className="column-container__text">
-          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
-        </div>
-      </div>
-      <div className="three-columns__column-container">
-        <div className="column-container__numberbox">
-          7
-        </div>
-        <div className="column-container__description">
-          ZORGANIOWANYCH ZBIÓREK  
-        </div>
-        <div className="column-container__text">
-          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.    
+        <div className="three-columns__column-container">
+          <div className="column-container__numberbox">7</div>
+          <div className="column-container__description">
+            ZORGANIOWANYCH ZBIÓREK
+          </div>
+          <div className="column-container__text">
+            Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+            enim a elit viverra elementuma. Aliquam erat volutpat.
+          </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
-}
+};
 
-export default ThreeColumns
+export default ThreeColumns;
