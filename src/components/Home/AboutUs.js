12ad3c6448bad5f422c2ae7f797f8b2e5ad32218
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-20 22:43:29 +0100

    AboutUs component code format

--- src/components/Home/AboutUs.js
+++ src/components/Home/AboutUs.js
@@ -1,26 +1,25 @@
 import React from "react";
 
-
 const AboutUs = () => {
-    
-return (
+  return (
     <>
-        <div className="about-us-section" id="about">
-            <div className="about-us-container__exterior-container">
-                <div className="about-us-container__about-content">
-                    <div className="about-content__title">O nas</div>
-                    <div className="about-content__decoration"></div>
-                    <div className="about-content_text">
-                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. 
-                    Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                    </div>
-                    <div className="about-content__signature"></div>
-                </div>
+      <div className="about-us-section" id="about">
+        <div className="about-us-container__exterior-container">
+          <div className="about-us-container__about-content">
+            <div className="about-content__title">O nas</div>
+            <div className="about-content__decoration"></div>
+            <div className="about-content_text">
+              Nori grape silver beet broccoli kombu beet greens fava bean potato
+              quandong celery. Bunya nuts black-eyed pea prairie turnip leek
+              lentil turnip greens parsnip.
             </div>
-            <div className="about-us-section__picture-container"></div>
+            <div className="about-content__signature"></div>
+          </div>
         </div>
+        <div className="about-us-section__picture-container"></div>
+      </div>
     </>
-);
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
